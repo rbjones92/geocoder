dbdc6888d8202727850fe93e9278858c49086d0b
Robert Jones <robertbjones92@gmail.com>
2023-02-02 16:33:00 -0800

    use faster apply method for geocoder

---
+++ geocode_apply.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import geocoder 
+key = 'ArxnM6fNPCfN_gfZQyXoDYreLKQInbzll9F_t4hMmRZl0eVmC3jJ3NXPGnUHtwuq'
+
+
+def geolocate_lat(ServiceAddress,ServiceCity,ServiceZipCode,ServiceState):
+    
+    full_address = f'{ServiceAddress}, {ServiceCity}, {ServiceZipCode}, {ServiceState}'
+
+    try:
+        g = geocoder.bing(full_address,key=key)
+        results = g.json
+        lat = results['lat']
+        print(f'found latitude {lat}')
+        return lat
+
+    except:
+            print(f'Could not find {full_address}')
+            return 
+
+
+def geolocate_long(ServiceAddress,ServiceCity,ServiceZipCode,ServiceState):
+      
+    full_address = f'{ServiceAddress}, {ServiceCity}, {ServiceZipCode}, {ServiceState}'
+
+    try:
+        g = geocoder.bing(full_address,key=key)
+        results = g.json
+        lng = results['lng']
+        print(f'found longitude {lng}')
+        return lng
+
+    except:
+            print(f'Could not find {full_address}')
+            return
+
+
+df = pd.DataFrame({'ServiceAddress': '10397 PIERI CT','ServiceCity':'MOSS LANDING','ServiceZipCode':'95039','ServiceState':'CA'},index=['test_address'])
+
+df['latidude'] = df.apply(lambda x: geolocate_lat(x['ServiceAddress'], x['ServiceCity'], x['ServiceZipCode'], x['ServiceState']), axis=1)
+
+df['longitude'] = df.apply(lambda x: geolocate_long(x['ServiceAddress'], x['ServiceCity'], x['ServiceZipCode'], x['ServiceState']), axis=1)
+
+print(df)
+
